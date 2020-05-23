22c5c8937706f85938b3ba18bf8dad31292288f4
Mugesh Kannan <mugeshk1171@gmail.com>
2020-05-23 13:31:35 +0530

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -7,16 +7,18 @@ import tensorflow as tf
 from PIL import Image
 physical_devices=tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0],True)
+from detect_face import face_extract
 
 from sklearn.preprocessing import Normalizer
+normalizer = Normalizer(norm='l2')
+
 from keras_facenet import FaceNet
 embedder = FaceNet()
 
-normalizer = Normalizer(norm='l2')
-from detect_face import face_extract
 
 with open('model.pkl', 'rb') as f:
-    clf = pickle.load(f)
+    model = pickle.load(f)
+ 
 file='5-celebrity-faces-dataset/val/madonna/httpassetsrollingstonecomassetsarticlemadonnadavidbowiechangedthecourseofmylifeforeversmallsquarexmadonnabowiejpg.jpg'
 img=plt.imread(file)
 face=face_extract(file,(160,160))
@@ -24,5 +26,5 @@ face_array = np.asarray(face)
 face_array=np.expand_dims(face_array,axis=0)
 emded=embedder.embeddings(face_array)
 emded=normalizer.fit_transform(emded)
-pred=clf.predict(emded)
+pred=model.predict(emded)
 print(pred)
