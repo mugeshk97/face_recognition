344371d2850dbbe677456b5e9b583c7aa967e0cc
Mugesh Kannan <mugeshk1171@gmail.com>
2020-05-23 16:31:50 +0530

    Update 04-recognizer.py

--- 04-recognizer.py
+++ 04-recognizer.py
@@ -7,24 +7,23 @@ import tensorflow as tf
 from PIL import Image
 physical_devices=tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0],True)
-from detect_face import face_extract
 
 from sklearn.preprocessing import Normalizer
-normalizer = Normalizer(norm='l2')
-
 from keras_facenet import FaceNet
 embedder = FaceNet()
 
-
+normalizer = Normalizer(norm='l2')
+from detect_face import face_extract
+class_label=['Ben Affleck','Elton John','Jerry Seinfeld','Madonna','Mindy Kaling']
 with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
- 
-file='5-celebrity-faces-dataset/val/madonna/httpassetsrollingstonecomassetsarticlemadonnadavidbowiechangedthecourseofmylifeforeversmallsquarexmadonnabowiejpg.jpg'
+    clf = pickle.load(f)
+file='dataset/val/madonna/httpassetsrollingstonecomassetsarticlemadonnadavidbowiechangedthecourseofmylifeforeversmallsquarexmadonnabowiejpg.jpg'
 img=plt.imread(file)
 face=face_extract(file,(160,160))
 face_array = np.asarray(face)
 face_array=np.expand_dims(face_array,axis=0)
 emded=embedder.embeddings(face_array)
 emded=normalizer.fit_transform(emded)
-pred=model.predict(emded)
-print(pred)
+pred=clf.predict(emded)
+label=class_label[pred.argmax()]
+print(label)
